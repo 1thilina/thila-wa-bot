075b6467c799d1c96c6a2937edddf1fc6eed5434
1thilina <148460963+1thilina@users.noreply.github.com>
2025-08-15 07:30:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Hf5gQTKZ#abji5cg-MCGKYc66cMu-970LO9PwUd-PQ9tCpdUT8XU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/1thilina/thila-wa-bot/blob/main/images/bot.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 THILA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
